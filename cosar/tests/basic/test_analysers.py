934c50f1f2bf49d9f6a1ea5f48329f8f889682fe
Mark Muetzelfeldt <markmuetz@gmail.com>
2018-07-03 17:49:42 +0100

    Add real test using u-au197 test dir.

--- cosar/tests/basic/test_analysers.py
+++ cosar/tests/basic/test_analysers.py
@@ -1,4 +1,7 @@
+from mock import Mock, patch
+
 import omnium
+from omnium.setup_logging import setup_logger
 import cosar
 
 
@@ -6,5 +9,17 @@ def test_omnium_version():
     omnium.omnium_main(['omnium', 'version'], 'cosar_testing')
 
 
-def test_cosar_analysis_classes():
+@patch('os.path.exists')
+@patch('os.makedirs')
+def test_cosar_analysis_classes(mock_makedirs, mock_exists):
     assert len(cosar.analysis_classes) > 0
+    setup_logger(debug=True, colour=False)
+    mock_exists.return_value = False
+    suite = Mock()
+    task = Mock()
+    suite.check_filename_missing.return_value = False
+    task.filenames = ['/a/b/c.txt']
+    task.output_filenames = ['/a/b/c.out']
+    for analysis_class in cosar.analysis_classes:
+        analyser = analysis_class(suite, task)
+        assert analyser
---
+++ cosar/tests/test_suite_u-au197/test_run_all_analysis.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+import omnium
+
+
+def setup_module():
+    suite_dir = os.getenv('COSAR_SUITE_UAU197_DIR', None)
+    assert suite_dir, 'Env set COSAR_SUITE_UAU197_DIR to your u-au197 test suite'
+    assert os.path.exists(suite_dir)
+    if os.path.exists(os.path.join(suite_dir, 'omnium_output')):
+        shutil.rmtree(os.path.join(suite_dir, 'omnium_output'))
+
+def test_01_run_cycle_analysis():
+    suite_dir = os.getenv('COSAR_SUITE_UAU197_DIR')
+    os.chdir(suite_dir)
+    omnium.omnium_main(['omnium', '-D', '-b', 'run', '-t', 'cycle', '--all', 'P6M_DP20'])
+
+def test_02_run_expt_analysis():
+    suite_dir = os.getenv('COSAR_SUITE_UAU197_DIR')
+    os.chdir(suite_dir)
+    omnium.omnium_main(['omnium', '-D', '-b', 'run', '-t', 'expt', '--all', 'P6M_DP20'])
+
+def test_03_run_suite_analysis():
+    suite_dir = os.getenv('COSAR_SUITE_UAU197_DIR')
+    os.chdir(suite_dir)
+    omnium.omnium_main(['omnium', '-D', '-b', 'run', '-t', 'suite', '--all', 'P6M_DP20'])
